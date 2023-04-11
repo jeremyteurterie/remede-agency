6333cf05cfb29d469e473ca21781f9424c33fbf1
Jeremy <jeremy.teurterie@gmail.com>
2023-04-11 16:30:38 +0700

    :sparkles: feat: add error page

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { useRouteError } from 'react-router-dom';
 
+import styles from '../styles/Error404.module.css';
+
 const Error404 = () => {
-  const error = useRouteError();
-  console.log(error);
   return (
-    <div id="error-page">
+    <div className={styles.errorpage}>
       <h1>Oops!</h1>
+      <h2>Error 404 : Page not found</h2>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
     </div>
   );
 };
