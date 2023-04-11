1e2744fd3c26e2be5eeaca54c729493dc7dbf1d0
Jeremy <jeremy.teurterie@gmail.com>
2023-04-11 20:28:06 +0700

    :memo: doc: add jsdoc for error page

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+// modules
 import { Link } from 'react-router-dom';
-
+// styles
 import styles from '../styles/Error404.module.css';
 
+/**
+ *Renders a custom 404 error page.
+ *@function
+ *@returns {JSX.Element} - The JSX code for the custom 404 error page.
+ */
 const Error404 = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.errorpage}>
-        <h1>Oops!</h1>
-        <h2>Error 404 : Page not found</h2>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <Link to="/">Return to the Home.</Link>
-        </p>
-      </div>
+    <div className={styles.errorpage}>
+      <h1>Oops!</h1>
+      <h2>Error 404 : Page not found</h2>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <Link to="/">Return to the Home.</Link>
+      </p>
     </div>
   );
 };
