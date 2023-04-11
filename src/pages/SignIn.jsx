ad281df0c5e0c2c430bb071faface1eb73cc5a55
Jeremy <jeremy.teurterie@gmail.com>
2023-04-11 21:41:04 +0700

    :sparkles: feat: add content signin page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+
+import styles from '../styles/SignIn.module.css';
 
 const SignIn = () => {
   return (
@@ -8,6 +14,29 @@ const SignIn = () => {
       <header>
         <Header />
       </header>
+      <main className={styles.mainsignin}>
+        <section className={styles.signincontent}>
+          <i className={styles.signinicon}>
+            <FontAwesomeIcon icon={faCircleUser} />
+          </i>
+          <h1>Sign In</h1>
+          <form>
+            <div className={styles.inputwrapper}>
+              <label for="username">Username</label>
+              <input type="text" id="username" />
+            </div>
+            <div className={styles.inputwrapper}>
+              <label for="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div className={styles.inputremember}>
+              <input type="checkbox" id="remember-me" />
+              <label for="remember-me">Remember me</label>
+            </div>
+            <button className="signinbutton">Sign In</button>
+          </form>
+        </section>
+      </main>
       <footer>
         <Footer />
       </footer>
