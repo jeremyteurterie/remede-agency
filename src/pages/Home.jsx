1c1f689f2ba4f91a5ccd6bc9bc83965c989ade6a
Jeremy <jeremy.teurterie@gmail.com>
2023-04-11 17:41:52 +0700

    :sparkles: feat: add footer.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <header>
+        <Header />
+      </header>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   );
 };
 
