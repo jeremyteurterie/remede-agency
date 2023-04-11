70e8bc579a86e593a349f18313e712c0a8fc6b91
Jeremy <jeremy.teurterie@gmail.com>
2023-04-11 20:40:39 +0700

    :sparkles: feat: add header & footer components to signin page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const SignIn = () => {
-  return <div></div>;
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
 };
 
 export default SignIn;
