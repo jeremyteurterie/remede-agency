9323d5aab1475440e83d898439d34af2d4818583
Jeremy <jeremy.teurterie@gmail.com>
2023-04-11 17:08:55 +0700

    :bug: feat: add container for style

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -5,13 +5,15 @@ import styles from '../styles/Error404.module.css';
 
 const Error404 = () => {
   return (
-    <div className={styles.errorpage}>
-      <h1>Oops!</h1>
-      <h2>Error 404 : Page not found</h2>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <Link to="/">Return to the Home.</Link>
-      </p>
+    <div className={styles.container}>
+      <div className={styles.errorpage}>
+        <h1>Oops!</h1>
+        <h2>Error 404 : Page not found</h2>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <Link to="/">Return to the Home.</Link>
+        </p>
+      </div>
     </div>
   );
 };
