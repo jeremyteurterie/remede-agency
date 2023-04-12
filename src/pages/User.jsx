8603fd176e996ddedfbfd114682ce4e4178a23ec
Jeremy <jeremy.teurterie@gmail.com>
2023-04-12 10:18:36 +0700

    :sparkles: feat: add user page

---
+++ src/pages/User.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+import styles from '../styles/User.module.css';
+
+const User = () => {
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <div className={styles.main}>
+        <div className={styles.mainuser}>
+          <div className={styles.header}>
+            <h1>
+              Welcome back
+              <br />
+              Tony Jarvis!
+            </h1>
+            <button className={styles.editbutton}>Edit Name</button>
+          </div>
+          <h2 className={styles.sronly}>Accounts</h2>
+          <section className={styles.account}>
+            <div className={styles.accountcontentwrapper}>
+              <h3 className={styles.accounttitle}>
+                Argent Bank Checking (x8349)
+              </h3>
+              <p className={styles.accountamount}>$2,082.79</p>
+              <p className={styles.accountamountdescription}>
+                Available Balance
+              </p>
+            </div>
+            <div className={styles.accountcontentwrapper + ' ' + styles.cta}>
+              <button className={styles.transactionbutton}>
+                View transactions
+              </button>
+            </div>
+          </section>
+          <section className={styles.account}>
+            <div className={styles.accountcontentwrapper}>
+              <h3 className={styles.accounttitle}>
+                Argent Bank Savings (x6712)
+              </h3>
+              <p className={styles.accountamount}>$10,928.42</p>
+              <p className={styles.accountamountdescription}>
+                Available Balance
+              </p>
+            </div>
+            <div className={styles.accountcontentwrapper + ' ' + styles.cta}>
+              <button className={styles.transactionbutton}>
+                View transactions
+              </button>
+            </div>
+          </section>
+          <section className={styles.account}>
+            <div className={styles.accountcontentwrapper}>
+              <h3 className={styles.accounttitle}>
+                Argent Bank Credit Card (x8349)
+              </h3>
+              <p className={styles.accountamount}>$184.30</p>
+              <p className={styles.accountamountdescription}>Current Balance</p>
+            </div>
+            <div className={styles.accountcontentwrapper + ' ' + styles.cta}>
+              <button className={styles.transactionbutton}>
+                View transactions
+              </button>
+            </div>
+          </section>
+        </div>
+      </div>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
+};
+
+export default User;
