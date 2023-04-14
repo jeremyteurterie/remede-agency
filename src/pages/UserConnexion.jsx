c1b1fb0e7a0cba5cd249810a9c049a244c48efd0
Jeremy <jeremy.teurterie@gmail.com>
2023-04-14 15:40:10 +0700

    :bookmark: release v0.3.0

--- src/pages/UserConnexion.jsx
+++ src/pages/UserConnexion.jsx
@@ -1,46 +1,25 @@
 import React from 'react';
 // modules
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 // components
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-// services
-import Auth from '../services/auth';
 // styles
 import styles from '../styles/UserConnexion.module.css';
 
 const UserConnexion = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const navigate = useNavigate();
-
-  const handleLogin = (event) => {
-    event.preventDefault();
-    Auth.login(email, password)
-      .then(() => {
-        navigate('/profil');
-      })
-      .catch((error) => {
-        setError(error.response.data.message);
-      });
-  };
-
   return (
     <>
       <header>
         <Header />
         <nav className={styles.mainnav}>
-          <Link to="/connexion">
-            <a className={styles.mainnavitem}>
-              <i className={styles.signinicon}>
-                <FontAwesomeIcon icon={faCircleUser} />
-              </i>
-              Sign In
-            </a>
+          <Link className={styles.mainnavitem} to="/connexion">
+            <i className={styles.signinicon}>
+              <FontAwesomeIcon icon={faCircleUser} />
+            </i>
+            Sign In
           </Link>
         </nav>
       </header>
@@ -51,33 +30,22 @@ const UserConnexion = () => {
               <FontAwesomeIcon icon={faCircleUser} />
             </i>
             <h1>Sign In</h1>
-            <form onSubmit={handleLogin}>
+            <form>
               <div className={styles.inputwrapper}>
-                <label htmlFor="email">Email</label>
-                <input
-                  type="text"
-                  id="email"
-                  value={email}
-                  onChange={(event) => setEmail(event.target.value)}
-                />
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" />
               </div>
               <div className={styles.inputwrapper}>
                 <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={(event) => setPassword(event.target.value)}
-                />
+                <input type="password" id="password" />
               </div>
               <div className={styles.inputremember}>
                 <input type="checkbox" id="remember-me" />
                 <label htmlFor="remember-me">Remember me</label>
               </div>
-              {error && <div className={styles.error}>{error}</div>}
-              <button type="submit" className={styles.signinbutton}>
-                Sign In
-              </button>
+              <Link to="/profil">
+                <button className={styles.signinbutton}>Sign In</button>
+              </Link>
             </form>
           </section>
         </main>
