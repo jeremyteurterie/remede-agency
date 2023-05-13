f42918212277033e8317169e50c7b3c22c47bb43
Jeremy <jeremy.teurterie@gmail.com>
2023-05-13 16:57:39 +0700

    update

--- backend/server/database/connection.js
+++ backend/server/database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost:3001/argentBankDB';
+  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB';
 
 module.exports = async () => {
   try {
