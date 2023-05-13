780b6cb264959d910edc5b8d61506daf87c23e65
Jeremy <jeremy.teurterie@gmail.com>
2023-05-13 16:33:47 +0700

    chore: restructuration files

--- backend/server/database/connection.js
+++ backend/server/database/connection.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  process.env.DATABASE_URL || 'mongodb://localhost:3001/argentBankDB';
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+    await mongoose.connect(databaseUrl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log('Database successfully connected');
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+    console.error(`Database Connectivity Error: ${error}`);
+    throw new Error(error);
   }
-}
+};
